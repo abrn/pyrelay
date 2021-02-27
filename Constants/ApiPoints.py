083a383652952fd154f3344a965b161c882e9c79
him <lordzhhao@gmail.com>
2021-02-27 02:47:26 +0000

    Update ApiPoints.py

--- Constants/ApiPoints.py
+++ Constants/ApiPoints.py
@@ -1,9 +1,9 @@
-
-VERSION = "https://static.drips.pw/rotmg/production/current/version.txt"
-EQUIP = "https://static.drips.pw/rotmg/production/current/xml/Equip.xml"
+VERSION = "https://api.rotmg.network/exalt/version"
+EQUIP = "https://api.rotmg.network/exalt/xml/equips"
 CHAR = "https://www.realmofthemadgod.com/char/list?do_login=true&guid={}&password={}&game_net=Unity&play_platform=Unity&game_net_user_id="
 
 exaltHeaders = {
+    "Content-Type": "application/x-www-form-urlencoded",
     "User-agent": "UnityPlayer/2019.4.9f1 (UnityWebRequest/1.0, libcurl/7.52.0-DEV)",
     "X-Unity-Version": "2019.4.9f1"
-    }
+}
